25de8599ca281b43cabb50a4a641be7bfcd41329
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-05 10:37:45 +0100

    complite first task

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const numero = prompt("Inserisci il primo numero");
 const numeroDue = prompt("Inserisci il secondo Numero");
-console.log(numero, numeroDue);
-
-if () {
+4
+if (numero > numeroDue) {
+    console.log("Il numero maggiore e'" + numero);
+    
+    
+} else if (numeroDue > numero) {
+    console.log("Il numero maggiore e'" + numeroDue);
+    
+}else {
+    console.log("I numeri sono uguali" + numero);
     
 }

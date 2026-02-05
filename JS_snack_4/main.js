14774dfa40b1266e4b8b34eb2cf81ca78b31eebd
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-05 12:51:32 +0100

    fix the promt

--- JS_snack_4/main.js
+++ JS_snack_4/main.js
@@ -1,7 +1,7 @@
 const numbers = [];
 
 for(let i = 0; i < 6; i++){
-    let number = Number(prompt("Inserisci 6 numeri"))
+    let number = Number(prompt("Inserisci numeri"))
     if(number % 2 === 0){
         numbers.push(number);
     }

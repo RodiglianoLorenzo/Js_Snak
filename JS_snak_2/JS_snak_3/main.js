c16f16dc962ac7ed4fdc1e9dbde5eac00b3eb006
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-05 12:12:29 +0100

    complete snak 3

--- JS_snak_2/JS_snak_3/main.js
+++ JS_snak_2/JS_snak_3/main.js
@@ -1,1 +1,9 @@
-console.log("Worka");
+let somma = 0;
+for (let i = 0; i < 10; i++){
+    let numero = Number(prompt("Inserisci 10 numeri"))
+    somma = somma + numero;
+    
+}
+console.log("La somma di tutti i numeri e'",  somma);
+
+

c0585631ba59a559c608159de631ef36b9e161d0
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-02-05 11:48:21 +0100

    fix something in JS_snak_2

--- JS_snak_2/main1.js
+++ JS_snak_2/main1.js
@@ -3,10 +3,10 @@ const parola_due = prompt ("Inserisci un altra parola")
 
 
 if (parola.length < parola_due.length){
-    console.log("la prima parola e la piu corta " + parola + ' '+ parola_due);
+    console.log("la prima parola e la piu corta " `${parola} ${parola_due}` );
     
 } else if (parola_due.length < parola.length){
-    console.log("la seconda parola e piu corta della prima", + parola_due + ' ' + parola);
+    console.log("la seconda parola e piu corta della prima", `${parola_due} ${parola}`);
     
 } else {
     console.log("le due parole hanno la stezza lunghezza ");
